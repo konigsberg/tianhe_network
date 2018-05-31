18ab6c92f6ef4b5bb8fd9642683e42cfb1c8d4b2
Jack Wang <wangzhao_zju@hotmail.com>
2018-05-31 13:27:18 +0800

    reorganize

--- tests/base_nrc/THNode.cc
+++
@@ -1,9 +0,0 @@
-#include "../../th2/node.h"
-
-class THNode : public Node {
-protected:
-  int32_t get_next_port() override { return (getIndex() % 32) % 12; };
-  int32_t get_node_num() override { return 18432; };
-};
-
-Define_Module(THNode);
--- th2/sim.h
+++
@@ -1,8 +0,0 @@
-#include "macros.h"
-
-constexpr double warm_up_time = 0.000000;
-constexpr double record_start_time = sim_start_time + warm_up_time;
-constexpr double injection_rate = 1.0;
-constexpr bool debug_mode = true;
-constexpr traffic_patterns traffic_pattern = traffic_patterns::uniform;
-constexpr log_levels log_lvl = log_levels::debug;
